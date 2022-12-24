2f9caa7406be5b38da4937895f6db9a639213f90
salarmotevalli <salar.mo.ro@gmail.com>
2022-12-24 22:50:55 +0330

    connect to db in authentication

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Start authentication service")
+
+	conn := connectDB()
+	if conn == nil {
+		panic("Cannot connect to Postgres")
+	}
+
+	// set up config
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+
+	// -------
+	// Possibility of error; it possible to execute this function without starting db in docker.
+	// So for avoiding errors, we use connectDB function for handle error
+	// -------
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectDB() *sql.DB {
+	var counts int8
+	dsn := os.Getenv("DNS")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready ...  ")
+			counts++
+		} else {
+			log.Println("Connected to Postgres")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Println("Backing off for two seconds ...")
+		time.Sleep(2 * time.Second)
+
+	}
+}
